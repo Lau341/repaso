a05bf923841826e44049c4c2db9fe5cbd0816522
lauu <90933095+Lau341@users.noreply.github.com>
2025-03-10 12:37:50 -0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,7 @@
+## crear un programa que solicite al usuario ingresar un numero entero, si el numero es mayor a 10 mostrar por pantalla el numero 10, de lo contrario mostrar por pantalla fin del programa
+
+num = int(Input("ingresar un numero: "))
+if num > 10:
+  print("10")
+else:
+  print ("fin del programa")
